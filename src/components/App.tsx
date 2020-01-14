0d0e50cf6dff1095b83b667147cd2017bdd680c1
VICT0R-S <vicsat93@gmail.com>
2020-01-14 22:40:10 +0900

    remove importing useState.

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,4 +1,4 @@
-import React , { useState , useEffect , useReducer } from 'react';
+import React , { useEffect , useReducer } from 'react';
 import '../css/App.css';
 import Header from './Header';
 import Movie from './Movie';
